65f0e7856537b24873aa9c250976ef3ab4888c8d
robertvenanzi <robertgvenanzi@gmail.com>
2019-01-22 17:37:21 -0800

    created actions for updateContact and getContact

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,10 +1,18 @@
-import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT } from './types';
+import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT, GET_CONTACT, UPDATE_CONTACT } from './types';
 import axios from 'axios';
 
 export const getContacts = () => async dispatch => {
     const res = await axios.get('https://jsonplaceholder.typicode.com/users')
     dispatch({
         type: GET_CONTACTS,
+        payload: res.data
+    });
+};
+
+export const getContact = (id) => async dispatch => {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    dispatch({
+        type: GET_CONTACT,
         payload: res.data
     });
 };
@@ -31,4 +39,12 @@ export const deleteContact = (id) => async dispatch => {
         });
     }
 
+};
+
+export const updateContact = contact => async dispatch => {
+    const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${contact.id}`, contact);
+    dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+    });
 };
