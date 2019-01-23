5643555c8b05f80a7118a4b67ea079c6d0b69493
robertvenanzi <robertgvenanzi@gmail.com>
2019-01-22 18:20:09 -0800

    changed route link for home in the header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,13 +7,13 @@ const Header = props => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <a href="/contact-manager-redux/" className="navbar-brand">
           {branding}
         </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to="/" className="nav-link">
+              <Link to="/contact-manager-redux/" className="nav-link">
                 <i className="fas fa-home" /> Home
               </Link>
             </li>
